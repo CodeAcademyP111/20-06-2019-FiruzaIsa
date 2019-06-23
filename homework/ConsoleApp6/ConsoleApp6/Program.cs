172d76da25a53bdb40f1e0c9a62521cb29fbe5e2
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-06-23 22:34:26 +0400

    Add files via upload

---
+++ homework/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] a = { 2, 3, 4, 5, 6 };   //tapwiriq 1
+            //Average(a);
+
+
+            //string sentence = Console.ReadLine();        //tapwiriq 2
+            //string result = FindTheSpaceBetweenWords(sentence);
+            //Console.WriteLine(result);
+
+
+
+            //string str = "hello world";      //tapwiriq3
+            //string LastResult = ReversedString(str);
+            //Console.WriteLine(LastResult);
+
+        }
+
+
+
+        #region tapwiriq1
+        //static void Average(int[] arr)     
+        //{
+        //   int count = 0;
+        //    foreach (int numb in arr)
+        //    {
+
+        //        count += numb;
+        //       }
+        //    int result = count / arr.Length;
+        //    Console.WriteLine(result);
+        //}
+        #endregion
+
+        #region tapwiriq2
+        //static string FindTheSpaceBetweenWords(string phrase)
+        //{
+        //    int count = 0;
+
+        //    for (int i = 0; i < phrase.Length; i++)
+        //    {
+        //        if (phrase[i] == ' ')
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return "The count of space between string :" + count;
+        //}
+        #endregion
+
+        #region tapwiriq3
+        // static string ReversedString(string reverse)
+        //{
+        //    string result = "";
+        //    for (int i = reverse.Length-1; i >= 0; i--) 
+        //    {
+        //        result += reverse[i];
+        //    }
+
+        //    return result;
+        //}
+        #endregion
+
+
+
+
+
+
+
+    }
+}
+
+    
+
+
+
